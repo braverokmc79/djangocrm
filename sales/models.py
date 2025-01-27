fdcb9c41bd16b42d1d0fd6d51626f3d0aa643480
braverokmc79 <braverokmc79@gmail.com>
2025-01-27 14:50:59 +0900

    4. 모델 (Model) 데이터 베이스 필드 추가 하기

--- sales/models.py
+++ sales/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 class Sales(models.Model):
+    경로_선택 =(
+        ('nr', '네이버'),
+        ('shop2', '샵투월드'),
+        ('news', '이메일뉴스레터'),
+    )
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
 
+    기존고객 = models.BooleanField(default=False)
+    유입경로 = models.CharField(choices=경로_선택, max_length=200 )
+    인물사진 = models.ImageField(blank=True, null=True)
+    파일 = models.FileField(blank=True, null=True)
 
