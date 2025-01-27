a2139d5e99366ede93ad2492091800ac44c9fc7b
braverokmc79 <braverokmc79@gmail.com>
2025-01-27 15:01:46 +0900

    5.외래 키(Foreign Key) 정리

--- sales/models.py
+++ sales/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 
-class Sales(models.Model):
-    경로_선택 =(
-        ('nr', '네이버'),
-        ('shop2', '샵투월드'),
-        ('news', '이메일뉴스레터'),
-    )
+class Sale(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+    
 
-    기존고객 = models.BooleanField(default=False)
-    유입경로 = models.CharField(choices=경로_선택, max_length=200 )
-    인물사진 = models.ImageField(blank=True, null=True)
-    파일 = models.FileField(blank=True, null=True)
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
