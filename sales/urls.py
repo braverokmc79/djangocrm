af0cd35656b35fa2f4de5f8cb508bd4bb5e294a3
braverokmc79 <braverokmc79@gmail.com>
2025-02-01 08:43:58 +0900

    20. Django URL namespace 사용하기

--- sales/urls.py
+++ sales/urls.py
@@ -1,20 +1,15 @@
 from django.urls import path
 from .views import 세일목록, 세일상세, 세일_입력 , 세일_업데이트, 세일_지우기
-
+from django.urls.conf import include
 
 app_name="홈페이지"
 
 urlpatterns = [
-    path("", 세일목록),
-    
+    path("", 세일목록 , name="목록"),    
     #http://127.0.0.1:8000/홈페이지/2/
-    path("<int:pk>/", 세일상세),
+    path("<int:pk>/", 세일상세 , name="상세"),
+    path("<int:pk>/업데이트/", 세일_업데이트, name="업뎃"),
+    path("<int:pk>/지우기/", 세일_지우기, name="지우기"),
+    path("make/", 세일_입력, name="생성"),
 
-    path("<int:pk>/업데이트/", 세일_업데이트),
-
-    path("<int:pk>/지우기/", 세일_지우기),
-
-    path("make/", 세일_입력),
-
-    
 ]
