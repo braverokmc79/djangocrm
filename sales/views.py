9f7a1b557f6bb5856d7caffdfac619271fd8de9a
braverokmc79 <braverokmc79@gmail.com>
2025-01-31 13:07:43 +0900

    16. 장고의 폼(Form) 2.일반 폼- 입력값 유효성 검증과 cleaned_data, redirect

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Sale
+from django.shortcuts import render , redirect
+from .models import Sale , Person
 from .forms import SaleForm
 
 def 세일목록(request):
@@ -27,7 +27,27 @@ def 세일상세(request,pk):
 
 
 def 세일_입력(request):
-    print(request.POST)
+    폼 =SaleForm()
+    if request.method == "POST":
+       print("포스트 메소드로 왔네요")
+       폼 = SaleForm(request.POST)
+       if 폼.is_valid():
+          print("유효하네요.")
+          print("폼 cleaned_data : ",폼.cleaned_data)
+          first_name =폼.cleaned_data["first_name"]
+          last_name =폼.cleaned_data["last_name"]
+          age =폼.cleaned_data["age"]
+          person=Person.objects.first()
+
+          Sale.objects.create(
+             first_name = first_name,
+             last_name = last_name,
+             age = age,
+             person = person
+          )
+          print("세일이 입력 되었습니다.")          
+          return redirect("/홈페이지")
+       
     context={
         "폼키" : SaleForm
     }
