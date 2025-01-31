637d5d3e5dfc7c8bd71dedb9716d87abaca2b5ef
braverokmc79 <braverokmc79@gmail.com>
2025-01-31 10:47:43 +0900

    11.HTML에 생명을! 콘텍스트(context)

--- sales/views.py
+++ sales/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import 아이디
 
 
 def 홈페이지(request):
     #return render(request, 'newfolder/아무거나1.html')
-    return render(request, '아무거나2.html')
+    드실분=아이디.objects.all()
+    context={
+        "메뉴명" :"짜장",
+        "가격" : "700원",
+        "손님" : 드실분
+    }
+    return render(request, '아무거나2.html',context)
