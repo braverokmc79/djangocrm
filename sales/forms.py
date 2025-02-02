745ddac795f22f2a46b2fb78067a1717692b78f4
braverokmc79 <braverokmc79@gmail.com>
2025-02-02 10:54:38 +0900

    24. Django + tailblocks 디자인 프레임워크 적용 화면 꾸미기 3

--- sales/forms.py
+++ sales/forms.py
@@ -10,6 +10,25 @@ class SaleModelForm(forms.ModelForm):
             "age",
             "person"
         )
+        styleInput="w-full max-w-sm border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:border-indigo-400 transition"
+        widgets ={
+            "first_name": forms.TextInput(attrs={
+                "class": styleInput,
+                "placeholder": "이름을 입력하세요",
+            }),
+            "last_name": forms.TextInput(attrs={
+                "class": styleInput,
+                "placeholder": "성을 입력하세요",
+            }),
+            "age": forms.NumberInput(attrs={
+                "class": styleInput,
+                "placeholder": "나이를 입력하세요",
+            }),
+            "person": forms.Select(attrs={
+                "class": styleInput,                
+            }),
+        }
+
 
 class SaleForm(forms.Form):
     first_name = forms.CharField(max_length=30)
